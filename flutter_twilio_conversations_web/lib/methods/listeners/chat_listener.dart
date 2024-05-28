57546ca7bec242251038616cb690adad17935a36
NicParmee <nicp@kena.health>
2024-05-28 16:20:03 +0200

    code refactor chat_listener

--- flutter_twilio_conversations_web/lib/methods/listeners/chat_listener.dart
+++ flutter_twilio_conversations_web/lib/methods/listeners/chat_listener.dart
@@ -3,10 +3,8 @@ import 'dart:js_util';
 import 'package:flutter_twilio_conversations/flutter_twilio_conversations.dart';
 import 'package:flutter_twilio_conversations_web/flutter_twilio_conversations_web.dart';
 import 'package:flutter_twilio_conversations_web/interop/classes/channel.dart';
-import 'package:flutter_twilio_conversations_web/interop/classes/channel.dart';
 import 'package:flutter_twilio_conversations_web/interop/classes/client.dart'
     as TwilioChatClient;
-import 'package:flutter_twilio_conversations_web/interop/classes/js_map.dart';
 import 'package:flutter_twilio_conversations_web/interop/classes/js_map.dart';
 import 'package:flutter_twilio_conversations_web/methods/listeners/base_listener.dart';
 import 'package:flutter_twilio_conversations_web/methods/mapper.dart';
@@ -22,7 +20,7 @@ class ChatClientEventListener extends BaseListener {
     this._chatClientStreamController,
   );
 
-  void addListeners() async {
+  void addListeners() {
     debug('Adding chatClientEventListeners for ${_client.connectionState}');
     _on('connectionStateChanged', connectionStateChange);
     _on('stateChanged', stateChanged);
@@ -120,7 +118,6 @@ class ChatClientEventListener extends BaseListener {
 
   void conversationJoined(dynamic channel) async {
     debug('Conversation Joined ChatClient Event');
-
     sendEvent('channelAdded', {
       "channel": Mapper.channelToMap(pluginInstance, channel),
     });
