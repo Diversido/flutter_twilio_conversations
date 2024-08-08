1d4ae96d605ff552bde7f9e3daf8711977423ef2
Martin Trollip <13981100+martintrollip@users.noreply.github.com>
2024-08-08 11:17:32 +0200

    Web: Get messages backwards

--- flutter_twilio_conversations_web/lib/interop/classes/message_builder.dart
+++ flutter_twilio_conversations_web/lib/interop/classes/message_builder.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_twilio_conversations_web/interop/classes/js_map.dart';
+import 'package:flutter_twilio_conversations_web/interop/classes/unsent_message.dart';
 import 'package:js/js.dart';
 
 @JS('Twilio.Conversations.MessageBuilder')
@@ -10,5 +11,5 @@ class MessageBuilder {
   external MessageBuilder setAttributes(JSMap attributes);
   external MessageBuilder addMedia(FormData media);
   external MessageBuilder withOptions(JSMap options);
-  external dynamic build();
+  external UnsentMessage build();
 }
