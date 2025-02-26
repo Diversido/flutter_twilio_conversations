733a66a6fc9a2976be7c92c98912646ea4b598d3
Martin Trollip <13981100+martintrollip@users.noreply.github.com>
2025-02-26 11:33:54 +0200

    fix: Web - Handled empty tokens in unregisterForNotification

--- flutter_twilio_conversations_web/lib/methods/notifications_methods.dart
+++ flutter_twilio_conversations_web/lib/methods/notifications_methods.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_twilio_conversations_web/interop/classes/client.dart';
+import 'package:flutter_twilio_conversations_web/logging.dart';
 import 'package:js/js_util.dart';
 
 class NotificationsMethods {
@@ -17,6 +18,11 @@ class NotificationsMethods {
     required String channel,
     required String token,
   }) async {
+    if (token.isEmpty) {
+      Logging.debug('error: the parameter "token" was not given');
+      return;
+    }
+
     await promiseToFuture<void>(
       chatClient?.removePushRegistrations(channel, token),
     );
